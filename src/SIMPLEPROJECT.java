966f632d5c6d152dde8772643085548feb6ddf51
Ramya Raman <ramyarraman@gmail.com>
2022-06-21 17:14:23 -0500

    just practice

--- src/SIMPLEPROJECT.java
+++ src/SIMPLEPROJECT.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 
 public class SIMPLEPROJECT {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter a string");
+		String s4=sc.nextLine();
+		System.out.println("Please enter the character to remove");
+		String noNeed = sc.nextLine();
+		s4=s4.replace(noNeed,"");
+		System.out.println(s4);		
+		sc.close();
+	
+		
+		
 	}
 
 }
